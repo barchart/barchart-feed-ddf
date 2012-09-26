241f716b014e99d9d98089aff82d1bb95a6449b1
m-ehrenberg <mike@barchart.com>
2012-09-26 10:32:14 -0500

    logging

--- barchart-feed-ddf-message/src/main/java/com/barchart/feed/ddf/message/enums/DDF_TradeDay.java
+++ barchart-feed-ddf-message/src/main/java/com/barchart/feed/ddf/message/enums/DDF_TradeDay.java
@@ -297,7 +297,7 @@ public enum DDF_TradeDay implements EnumCodeByte, EnumByteOrdinal {
 				generated = todayDate;
 			}
 		} catch (Exception e) {
-			logger.error("error parsing date, day code = " + tradeDay.code);
+			logger.error("error parsing date, day code = " + tradeDay.code + " day = " + tradeDay.day);
 			e.printStackTrace();
 			generated = todayDate;
 		}
