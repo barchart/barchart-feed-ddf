5ffdaf3dd274a6c08216151965c72073a1031e21
glitchfield <gavin.litchfield@barchart.com>
2013-02-25 12:23:27 -0600

    ServiceDatabaseDDF added functionality

--- barchart-feed-ddf-instrument/src/main/java/com/barchart/feed/ddf/instrument/provider/ServiceDatabaseDDF.java
+++ barchart-feed-ddf-instrument/src/main/java/com/barchart/feed/ddf/instrument/provider/ServiceDatabaseDDF.java
@@ -1,10 +1,13 @@
 package com.barchart.feed.ddf.instrument.provider;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,7 +31,47 @@ public class ServiceDatabaseDDF implements DDF_DefinitionService {
 	}
 
 	@Override
-	public Instrument lookup(CharSequence symbol) {
+	public Instrument lookup(final CharSequence symbol) {
+		
+		return lookupBase(symbol);
+		
+	}
+
+	@Override
+	public Future<Instrument> lookupAsync(final CharSequence symbol) {
+		return new FutureTask<Instrument>(new SymbolLookup(symbol));
+	}
+
+	@Override
+	public Map<CharSequence, Instrument> lookup(
+			final Collection<? extends CharSequence> symbols) {
+		
+		final Map<CharSequence, Instrument> result = 
+				new HashMap<CharSequence, Instrument>();
+		
+		// Currently just doing serial lookup
+		for(final CharSequence symbol : symbols) {
+			result.put(symbol, lookupBase(symbol));
+		}
+		
+		return result;
+	}
+
+	@Override
+	public Map<CharSequence, Future<Instrument>> lookupAsync(
+			final Collection<? extends CharSequence> symbols) {
+		
+		final Map<CharSequence, Future<Instrument>> result =
+				new HashMap<CharSequence, Future<Instrument>>();
+		
+		for(final CharSequence symbol : symbols) {
+			result.put(symbol, new FutureTask<Instrument>(new SymbolLookup(symbol)));
+		}
+				
+		return result;
+	}
+	
+	private Instrument lookupBase(final CharSequence symbol) {
 		
 		if(symbol == null || symbol.length() == 0) {
 			return Instrument.NULL_INSTRUMENT;
@@ -50,27 +93,23 @@ public class ServiceDatabaseDDF implements DDF_DefinitionService {
 			cache.put(symbol, instrument);
 			return instrument;
 		}
+	}
+	
+	private class SymbolLookup implements Callable<Instrument> {
+
+		private final CharSequence symbol;
 		
-	}
-
-	@Override
-	public Future<Instrument> lookupAsync(CharSequence symbol) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Map<CharSequence, Instrument> lookup(
-			Collection<? extends CharSequence> symbols) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Map<CharSequence, Future<Instrument>> lookupAsync(
-			Collection<? extends CharSequence> symbols) {
-		// TODO Auto-generated method stub
-		return null;
+		public SymbolLookup(final CharSequence symbol) {
+			this.symbol = symbol;
+		}
+		
+		@Override
+		public Instrument call() throws Exception {
+			
+			return lookupBase(symbol);
+			
+		}
+		
 	}
 
 }
