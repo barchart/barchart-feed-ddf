3d8398f638276c3749fb6be8403849be47e04c5f
glitchfield <gavin.litchfield@barchart.com>
2013-03-18 10:45:37 -0500

    Changes

--- barchart-feed-ddf-client/src/main/java/com/barchart/feed/client/provider/BarchartFeedReceiver.java
+++ barchart-feed-ddf-client/src/main/java/com/barchart/feed/client/provider/BarchartFeedReceiver.java
@@ -26,7 +26,6 @@ package com.barchart.feed.client.provider;
 import java.io.File;
 import java.io.IOException;
 import java.util.Set;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
