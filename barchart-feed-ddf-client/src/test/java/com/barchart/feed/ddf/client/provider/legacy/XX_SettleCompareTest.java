2e80cb03ad9e74a318a691909d43acc1ea5aa609
glitchfield <gavin.litchfield@barchart.com>
2013-06-24 12:45:07 -0500

    Added settle test

--- barchart-feed-ddf-client/src/test/java/com/barchart/feed/ddf/client/provider/legacy/XX_SettleCompareTest.java
+++ barchart-feed-ddf-client/src/test/java/com/barchart/feed/ddf/client/provider/legacy/XX_SettleCompareTest.java
@@ -11,36 +11,38 @@ import com.barchart.feed.base.market.enums.MarketEvent;
 import com.barchart.feed.base.market.enums.MarketField;
 import com.barchart.feed.ddf.instrument.provider.DDF_InstrumentProvider;
 
-public class XX_SessionCompareTest {
+public class XX_SettleCompareTest {
 	
-	final static String SYMBOL = "ESU3";
+	// KCU13
+	final static String SYMBOL = "ZCZ3";
 	
 	public static void main(final String[] args) throws Exception {
-		
+	
 		final String username = System.getProperty("barchart.username");
 		final String password = System.getProperty("barchart.password");
 		
 		final TestableFeed feed = new TestableFeed(username, password);
 		
 		final MarketObserver<Session> callback = new MarketObserver<Session>() {
-
+	
 			@Override
 			public void onNext(final Session v) {
 				
-				System.out.println("AGENT: " +
-				v.instrument().symbol() + " " +
-				v.open().asDouble() + " " +
-				v.high().asDouble() + " " +
-				v.low().asDouble() + " " +
-				v.volume().asDouble() + " " +
-				v.previous().close().asDouble()
+				if(v.isSettled()) {
 				
-				);
-				
+					System.out.println("AGENT: " +
+					v.instrument().symbol() + " " +
+					"IS SETTLED: " + v.isSettled() + " " +
+					" @ " + v.settle() + " " +
+					" PREVIOUS = " +
+					v.previous().close().asDouble()
+					
+					);
+				}
 			}
 			
 		};
-		
+	
 		final ConnectionFuture<Feed> start = feed.startup();
 		
 		start.get();
@@ -53,49 +55,52 @@ public class XX_SessionCompareTest {
 		
 		feed.addTaker(new SessionTaker(new Instrument[]{inst}));
 		
-		Thread.sleep(700000);
-		
+		Thread.sleep(7000000);
+	
 	}
 	
 	public static class SessionTaker implements 
 			MarketTaker<com.barchart.feed.base.market.api.Market> {
 
-			final Instrument[] instruments;
+		final Instrument[] instruments;
 	
-			public SessionTaker(final Instrument[] instruments) {
-				this.instruments = instruments;
-			}
+		public SessionTaker(final Instrument[] instruments) {
+			this.instruments = instruments;
+		}
 	
-			@Override
-			public MarketField<com.barchart.feed.base.market.api.Market> bindField() {
-				return MarketField.MARKET;
+		@Override
+		public MarketField<com.barchart.feed.base.market.api.Market> bindField() {
+			return MarketField.MARKET;
+		}
+	
+		@Override
+		public MarketEvent[] bindEvents() {
+			return new MarketEvent[] {MarketEvent.NEW_BAR_CURRENT};
+		}
+	
+		@Override
+		public Instrument[] bindInstruments() {
+			return instruments;
+		}
+	
+		@Override
+		public void onMarketEvent(MarketEvent event, Instrument instrument,
+				com.barchart.feed.base.market.api.Market v) {
+			
+			if(v.session().isSettled()) {
+			
+			System.out.println("TAKER: " + v.instrument().symbol() +  " " +
+					"IS SETTLED: " + v.session().isSettled() + " " +
+					" @ " + v.session().settle() + " " +
+					" PREVIOUS = " +
+					v.session().previous().close().asDouble()
+					
+					+ "\n");
+			
 			}
-
-			@Override
-			public MarketEvent[] bindEvents() {
-				return new MarketEvent[] {MarketEvent.NEW_BAR_CURRENT};
-			}
-
-			@Override
-			public Instrument[] bindInstruments() {
-				return instruments;
-			}
-
-			@Override
-			public void onMarketEvent(MarketEvent event, Instrument instrument,
-					com.barchart.feed.base.market.api.Market v) {
-				
-				System.out.println("TAKER: " + v.instrument().symbol() +  " " +
-						v.session().open().asDouble() + " " +
-						v.session().high().asDouble() + " " +
-						v.session().low().asDouble() + " " +
-						v.session().volume().asDouble() + " " +
-						v.session().previous().close().asDouble()
-						
-						+ "\n");
-				
-			}
-		
+			
+		}
+	
 	};
 
 }
