5ee7b8af9ee2b6c3800869435637ae08cf4d6b2e
glitchfield <gavin.litchfield@barchart.com>
2013-06-24 14:10:04 -0500

    Updated reciever

--- barchart-feed-ddf-client/src/main/java/bench/Receiver.java
+++ barchart-feed-ddf-client/src/main/java/bench/Receiver.java
@@ -7,15 +7,16 @@
  */
 package bench;
 
-import com.barchart.feed.api.enums.MarketSide;
+import com.barchart.feed.api.Agent;
+import com.barchart.feed.api.Feed;
+import com.barchart.feed.api.MarketObserver;
+import com.barchart.feed.api.connection.ConnectionFuture;
+import com.barchart.feed.api.model.PriceLevel;
+import com.barchart.feed.api.model.data.Market;
+import com.barchart.feed.api.model.data.Session;
+import com.barchart.feed.api.model.data.TopOfBook;
 import com.barchart.feed.api.model.meta.Instrument;
-import com.barchart.feed.base.book.api.MarketBookEntry;
-import com.barchart.feed.base.book.api.MarketBookTop;
-import com.barchart.feed.base.market.api.Market;
-import com.barchart.feed.base.market.api.MarketTaker;
-import com.barchart.feed.base.market.enums.MarketEvent;
-import com.barchart.feed.base.market.enums.MarketField;
-import com.barchart.util.values.util.ValueUtil;
+import com.barchart.feed.client.provider.BarchartFeed;
 
 /**
  * 
@@ -39,99 +40,61 @@ public class Receiver {
 	 */
 	public static void main(final String... args) throws Exception {
 
-		final int INST_START = 2;
+		if (args == null || args.length < 3) {
+			throw new RuntimeException("Bad arguments passed to main");
+		}
+		
+		final Feed feed = new BarchartFeed(args[0], args[1]);
 
-		if (args == null) {
-			throw new RuntimeException("No arguments passed to main");
+		final MarketObserver<Market> observer = new MarketObserver<Market>() {
+
+			@Override
+			public void onNext(Market value) {
+				
+				final StringBuilder sb = new StringBuilder(value.instrument().marketGUID())
+					.append(" ")
+					.append(" EventTime=")
+					.append(value.updated());
+
+				final TopOfBook top = value.orderBook().topOfBook();
+				
+				PriceLevel entry = top.ask();
+				
+				if (!entry.isNull()) {
+					sb.append(" ASK TOP").append(" price=")
+							.append(entry.price().asDouble())
+							.append(" qty=").append(entry.size().asDouble());
+				}
+				
+				entry = top.bid();
+				
+				if (!entry.isNull()) {
+					sb.append(" BID TOP").append(" price=")
+							.append(entry.price().asDouble())
+							.append(" qty=").append(entry.size().asDouble());
+				}
+
+				System.out.println(sb.toString());
+
+				
+			}
+			
+		};
+		
+		final ConnectionFuture<Feed> start = feed.startup();
+		
+		start.get();
+		
+		final Agent myAgent = feed.newAgent(Market.class, observer);
+		
+		for(int i = 2; i < args.length; i++) {
+			myAgent.include(args[i]);
 		}
 
-//		final BarchartFeedReceiver client = new BarchartFeedReceiver();
-//
-//		final Instrument[] instruments = new Instrument[args.length
-//				- INST_START];
-//
-//		for (int i = INST_START; i < args.length; i++) {
-//			instruments[i - INST_START] = client.lookup(args[i]);
-//		}
-//
-//		if (args[0].equals("TCP")) {
-//			client.listenTCP(Integer.parseInt(args[1]), false);
-//		} else if (args[0].equals("UDP")) {
-//			client.listenUDP(Integer.parseInt(args[1]), false);
-//		} else {
-//			throw new RuntimeException("Bad protocol, expecting UDP or TCP");
-//		}
-//
-//		client.addAllMarketsTaker(TakerFactory.makeTaker(instruments));
-//
-//		System.in.read();
-//		client.shutdown();
-//		System.exit(0);
+		System.in.read();
+		feed.shutdown();
+		System.exit(0);
 
 	}
-
-//	private static class TakerFactory {
-//
-//		static MarketTaker<Market> makeTaker(
-//				final Instrument[] instruments) {
-//
-//			return new MarketTaker<Market>() {
-//
-//				@Override
-//				public MarketField<Market> bindField() {
-//					return MarketField.MARKET;
-//				}
-//
-//				@Override
-//				public MarketEvent[] bindEvents() {
-//
-//					return new MarketEvent[] { MarketEvent.MARKET_UPDATED };
-//
-//				}
-//
-//				@Override
-//				public Instrument[] bindInstruments() {
-//
-//					return instruments;
-//
-//				}
-//
-//				@Override
-//				public void onMarketEvent(final MarketEvent event,
-//						final Instrument instrument, final Market value) {
-//
-//					final StringBuilder sb = new StringBuilder(value.instrument().marketGUID())
-//							.append(" ")
-//							.append(event)
-//							.append(" EventTime=")
-//							.append(value.get(MarketField.MARKET_TIME)
-//									.asDateTime().toString());
-//
-//					final MarketBookTop top = value.get(MarketField.BOOK_TOP);
-//
-//					MarketBookEntry entry = top.side(MarketSide.ASK);
-//
-//					if (!entry.isNull()) {
-//						sb.append(" ASK TOP").append(" price=")
-//								.append(ValueUtil.asDouble(entry.priceValue()))
-//								.append(" qty=").append(entry.sizeValue().asLong());
-//					}
-//
-//					entry = top.side(MarketSide.BID);
-//
-//					if (!entry.isNull()) {
-//						sb.append(" BID TOP").append(" price=")
-//								.append(ValueUtil.asDouble(entry.priceValue()))
-//								.append(" qty=").append(entry.sizeValue().asLong());
-//					}
-//
-//					System.out.println(sb.toString());
-//
-//				}
-//
-//			};
-//
-//		}
-//	}
 
 }
