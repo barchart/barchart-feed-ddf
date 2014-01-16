17594bb1d216d0453e8530d2a2c811c3f4a98580
jjongsma <jeremy@barchart.com>
2014-01-16 14:27:13 -0600

    Merge

--- barchart-feed-ddf-test/src/test/java/com/barchart/feed/test/replay/TestXFSettle.java
+++ barchart-feed-ddf-test/src/test/java/com/barchart/feed/test/replay/TestXFSettle.java
@@ -7,12 +7,14 @@ import org.slf4j.LoggerFactory;
 import com.barchart.feed.api.Agent;
 import com.barchart.feed.api.MarketObserver;
 import com.barchart.feed.api.model.data.Market;
+import com.barchart.feed.api.model.data.Session;
+import com.barchart.feed.inst.provider.Exchanges;
 import com.barchart.util.value.api.Time;
 
 public class TestXFSettle {
-	
-	protected static final Logger log = LoggerFactory.getLogger(
-			TestXFSettle.class);
+
+	protected static final Logger log = LoggerFactory
+			.getLogger(TestXFSettle.class);
 
 	@Test
 	public void testXFSettle() throws Exception {
@@ -22,7 +24,7 @@ public class TestXFSettle {
 
 		final Agent agent = market.newAgent(Market.class, obs);
 
-		agent.include("XFH14");
+		agent.include(Exchanges.fromName("BMF"));
 
 		final FeedReplayer replayer =
 				new FeedReplayer(
@@ -32,23 +34,67 @@ public class TestXFSettle {
 
 	}
 
+	@Test
+	public void testXFSettle_JJ() throws Exception {
+
+		final BarchartMarketplaceReplay marketplace =
+				new BarchartMarketplaceReplay();
+
+		final SettleObserver so = new SettleObserver();
+		final Agent agent = marketplace.newAgent(Market.class, so);
+
+		agent.include(Exchanges.fromName("BMF"));
+
+		final FeedReplayer replayer =
+				new FeedReplayer(
+						FeedReplayer.class.getResource("/XF_20140113.txt"));
+
+		replayer.run(marketplace.maker());
+
+		System.out.println("Final settlement states:");
+		for (final String sym : new String[] {
+				"XFH14", "XFK14", "XFN14", "XFU14"
+		}) {
+			final Market snapshot = marketplace.snapshot(sym);
+			System.out.println(sym
+					+ ": settle="
+					+ snapshot.sessionSet()
+							.session(Session.Type.DEFAULT_CURRENT).settle()
+							.asDouble()
+					+ ", prevSettle="
+					+ snapshot.sessionSet()
+							.session(Session.Type.DEFAULT_PREVIOUS).settle()
+							.asDouble());
+		}
+
+	}
+
+	private static class SettleObserver implements MarketObserver<Market> {
+
+		@Override
+		public void onNext(final Market m) {
+		}
+
+	}
+
 	static MarketObserver<Market> obs = new MarketObserver<Market>() {
 
 		private Time updated = Time.NULL;
-		
+
 		@Override
 		public void onNext(final Market v) {
-			
-			if(updated.isNull()) {
+
+			if (updated.isNull()) {
 				updated = v.session().updated();
 				return;
 			}
-			
-			if(updated.compareTo(v.session().updated()) > 0) {
-				log.debug("Updated time less than previous time {}", 
-						updated.millisecond() - v.session().updated().millisecond());
+
+			if (updated.compareTo(v.session().updated()) > 0) {
+				log.debug("Updated time less than previous time {}",
+						updated.millisecond()
+								- v.session().updated().millisecond());
 			}
-			
+
 		}
 
 	};
