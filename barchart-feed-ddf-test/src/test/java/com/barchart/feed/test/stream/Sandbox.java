9fa9fe536e5d0c52b4ecb10d42e45a4520a334a8
jjongsma <jeremy@barchart.com>
2014-07-31 14:35:05 -0500

    Updated

--- barchart-feed-ddf-test/src/test/java/com/barchart/feed/test/stream/Sandbox.java
+++ barchart-feed-ddf-test/src/test/java/com/barchart/feed/test/stream/Sandbox.java
@@ -3,6 +3,8 @@ package com.barchart.feed.test.stream;
 import com.barchart.feed.api.MarketObserver;
 import com.barchart.feed.api.Marketplace;
 import com.barchart.feed.api.model.data.Market;
+import com.barchart.feed.api.model.data.Session.Type;
+import com.barchart.feed.api.model.data.SessionData;
 import com.barchart.feed.client.provider.BarchartMarketplace;
 
 public class Sandbox {
@@ -17,10 +19,30 @@ public class Sandbox {
 
 			@Override
 			public synchronized void onNext(final Market m) {
-				System.out.println(m.instrument().symbol() + ": " + m.lastPrice().price());
+
+				final SessionData current = m.sessionSet().session(Type.DEFAULT_CURRENT);
+
+				if (current != null) {
+					System.out.println("++ CURRENT");
+					System.out.println("OPEN: " + (current.open().isNull() ? "0" : current.open().toString()));
+					System.out.println("HIGH: " + (current.high().isNull() ? "0" : current.high().toString()));
+					System.out.println("LOW: " + (current.low().isNull() ? "0" : current.low().toString()));
+					System.out.println("CLOSE: " + (current.close().isNull() ? "0" : current.close().toString()));
+				}
+
+				final SessionData previous = m.sessionSet().session(Type.DEFAULT_PREVIOUS);
+
+				if (previous != null) {
+					System.out.println("++ PREVIOUS");
+					System.out.println("OPEN: " + (previous.open().isNull() ? "0" : previous.open().toString()));
+					System.out.println("HIGH: " + (previous.high().isNull() ? "0" : previous.high().toString()));
+					System.out.println("LOW: " + (previous.low().isNull() ? "0" : previous.low().toString()));
+					System.out.println("CLOSE: " + (previous.close().isNull() ? "0" : previous.close().toString()));
+				}
+
 			}
 
-		}, "IBM");
+		}, "CTZ14");
 
 		while (true) {
 			Thread.sleep(10000);
